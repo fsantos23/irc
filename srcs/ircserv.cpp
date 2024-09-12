48c311f05333192470429ddd28b7fbf162ba0ad7
Pedro Cristovao Veiga Correia <pveiga-c@c2r1s10.42lisboa.com>
2024-09-12 17:29:14 +0100

    update ircserv.cpp

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -3,32 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ircserv.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pviegas <pviegas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pveiga-c <pveiga-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 10:50:20 by pviegas           #+#    #+#             */
-/*   Updated: 2024/09/12 12:23:38 by pviegas          ###   ########.fr       */
+/*   Updated: 2024/09/12 17:25:28 by pveiga-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Server.hpp"
 
-int main(int ac, char **av)
+bool	invalid_arguments(int argc, char *argv[])
 {
-	if(ac != 3)
-	{
-		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
-        return 1;
+	if (argc != 3) {
+		std::cout << RED "Usage: ./ircserv <port> <password>" WHI << std::endl;
+		return (1);
 	}
-	//fazer check do port passado para verificar se é valido ou não
-	Server ser(std::atoi(av[1]), av[2]);
+
+	std::string	port_num(argv[1]);
+	if (port_num.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cerr << RED "Port Number Has To Be Numbers Only" WHI << std::endl;
+		return (1);
+	}
+	int	port = atoi(port_num.c_str());
+	if (port < 1024 || port > 65535) // confirmar min max port
+	{
+		std::cerr << RED "Port Number Has To Be In Range [1024, 65535]" WHI << std::endl;
+		return (1);
+	}
+	return (0);
+}
+//PCC
+int main(int argc, char **argv)
+{
+
+	if (invalid_arguments(argc, argv))
+		return (-1);
+
 	try
 	{
-		ser.initServer();
+		Server server = Server(atoi(argv[1]), std::string(argv[2]));
+		server.initServer();
 	}
-	catch (const std::exception &e)
-	{
-		std::cout << std::endl << RED << "Error: " << e.what() << WHI << std::endl;
-		return 1;
+	catch(std::exception &e) {
+		std::cerr << e.what() << std::endl;
+		exit(-1);
 	}
-	return 0;
+
+	return (0);
 }
